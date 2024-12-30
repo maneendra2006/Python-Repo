7fcfe21f803ea4e9e8733d0877f547f25adf7d18
maneendra2006 <147416105+maneendra2006@users.noreply.github.com>
2024-12-30 14:31:46 +0530

    Uploading code: Modulus.py

---
+++ Modulus.py
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+     {
+        Scanner A = new Scanner(System.in);
+        int a = A.nextInt();
+        int b = A.nextInt();
+        int c = a%b;
+        System.out.println(c);
+        }
+}
