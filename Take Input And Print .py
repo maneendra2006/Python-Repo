fc624a6ccc73899346f115d60a6bca38246d2ae8
maneendra2006 <147416105+maneendra2006@users.noreply.github.com>
2025-01-02 10:20:49 +0530

    Uploading code: Take Input And Print .py

---
+++ Take Input And Print .py
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TakeInputAndPrint 
+{
+ public static void main(String[] args) {
+        Scanner A = new Scanner(System.in);
+        String B = A.nextLine();
+        System.out.println(B);
+    }
+}
